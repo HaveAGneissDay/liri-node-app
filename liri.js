478b889762c8fce0bd5170698948019ad89bb322
David Cheng <dkcheng10@gmail.com>
2018-01-21 13:34:02 -0800

    basic psudocode structure

---
+++ liri.js
@@ -0,0 +1,13 @@
+//Call keys.js
+
+//Call Twitter API
+
+// ex: node liri.js my-tweets should call up tweets
+
+//Call Spotify API
+
+// ex: node liri.js spotify-this-song '<song name here>'
+
+//Call a Movie API
+
+// ex: node liri.js movie-this '<movie name here>'
