dea4355237f5730105988a775ec1f189fe8d2e16
David Cheng <dkcheng10@gmail.com>
2018-01-21 18:42:54 -0800

    created twitter keys

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+    consumer_key: '	uDhfRJZqRpqLfmy01pNs7sT5o',
+    consumer_secret: '0E2fDefpE3uo64C0DLTjPtWn6ah4xv3xNgSkwXJptnB9zpWQX9',
+    access_token_key: '	955238314403012608-CCffFfmVHCv789I3ASPtSoAzFpdoDPq',
+    access_token_secret: 'STZkcqyIl1JwDpdQfUXDmy35J4HOxu898mHh0V5Uwgngs',
+}
+
+module.exports = twitterKeys;
